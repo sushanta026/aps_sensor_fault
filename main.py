403c612f26267052c696a2bc2a6878bca82bcb71
sushanta026 <spsusantkumar@gmail.com>
2023-03-16 19:30:44 +0530

    updated files main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hi")
+from sensor.logger import logging
+from sensor.exception import SensorException
+import os,sys
+
+
+
+if __name__=="__main__":
+    try:
+        pass
+
+    except Exception as e:
+        print(e)
--- sensor/logger.py
+++ sensor/logger.py
@@ -1,4 +1,4 @@
-import logging
+'''import logging
 import os
 from datetime import datetime
 
@@ -16,4 +16,26 @@ logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[%(asktime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO
+)'''
+
+import logging
+import os,sys
+from datetime import datetime
+
+LOG_FILE_NAME = f"{datetime.now().strftime('%m%d%Y_%H%M%S')}.log"
+
+LOG_FILE_DIR = os.path.join(os.getcwd(),"log")
+
+#create folder if not exist
+os.makedirs(LOG_FILE_DIR,exist_ok=True)
+
+#Log file path
+LOG_FILE_PATH = os.path.join(LOG_FILE_DIR,LOG_FILE_NAME)
+
+
+logging.basicConfig(
+    filename = LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+
 )
